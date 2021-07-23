2cf519f85ec58945d97e0d8368f89ed2617b43e9
fureche <fureche@icfes.gov.co>
2021-07-23 15:41:22 -0500

    first commit

---
+++ src/main/java/com/util/message/queue/MessageApplication.java
@@ -0,0 +1,13 @@
+package com.util.message.queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageApplication.class, args);
+	}
+
+}
